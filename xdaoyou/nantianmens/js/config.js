6861fc360785240fcdd6c54898bdf90df328a9bd
joyfulsync <joyfulsync@gmail.com>
2025-03-28 03:04:41 +0800

    update xdaoyou

---
+++ xdaoyou/nantianmens/js/config.js
@@ -0,0 +1,3 @@
+var BaseUrl =  "http://106.54.88.119:8082";
+var WxUrl = "/";
+var ResourceUrl = '/';
---
+++ xdaoyou/shengxiaojis/js/config.js
@@ -0,0 +1,4 @@
+var BaseUrl =  "http://106.54.88.119:8082";
+var WxUrl = "/";
+var ResourceUrl = '/';
+var hallName = 'shengxiaojis';
