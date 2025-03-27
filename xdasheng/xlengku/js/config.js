25a8ca7ba43b705e5ec927b2bc467b842a216dec
joyfulsync <joyfulsync@gmail.com>
2025-03-28 03:06:07 +0800

    update xdasheng

---
+++ xdasheng/xlengku/js/config.js
@@ -0,0 +1,9 @@
+var BaseUrl =  "http://106.54.88.119:8082";
+var WxUrl = "/";
+var hallName = 'xlengku';
+var ResourceUrl = '';
+var Suffix = '.html';
+var _isTx = 0;
+
+document.write('<script type="text/javascript" src="fiesc/js/aes.js?v=1"> <\/script>');
+document.write('<script type="text/javascript" src="files/js/md5.js?v=1"> <\/script>');
